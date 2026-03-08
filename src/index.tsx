e2a6e9f64f066119447e6b714438fd8bb1fde3fa
aldo960 <jamjo1404@gmail.com>
2026-03-07 16:59:28 -0800

    Refactor React import and add type assertion

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-const rootElement = document.getElementById('root');
+
+const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+    <StrictMode>
+        <App />
+    </StrictMode>
+  );
